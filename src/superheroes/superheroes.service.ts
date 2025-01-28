8fa9172f4d659c2b4d89b03b210935e07c55e559
Dima <zimoykin@gmail.com>
2025-01-28 14:11:50 +0100

    add unit test, delete unuseless tests

--- src/superheroes/superheroes.service.ts
+++ src/superheroes/superheroes.service.ts
@@ -6,9 +6,22 @@ export class SuperheroesService {
   private readonly logger = new Logger(SuperheroesService.name);
   private superheroes: Array<ISuperhero> = [];
 
+  /**
+   * Get all superheroes in the database.
+   * @returns {Promise<Array<ISuperhero>>} A promise that resolves to a list of all superheroes.
+   */
   async getSuperheroes(): Promise<Array<ISuperhero>> {
     return this.superheroes;
   }
+
+  /**
+   * Adds a new superhero to the list, maintaining order based on humility score.
+   * The superhero will be inserted in the list at the position where the humility score is less than
+   * the given superhero's score. If no such position is found, the superhero is added to the end of the list.
+   *
+   * @param {ISuperhero} superhero - The superhero to be added.
+   * @returns {Promise<ISuperhero>} A promise that resolves to the added superhero.
+   */
 
   async addSuperhero(superhero: ISuperhero): Promise<ISuperhero> {
     this.logger.debug(`Adding superhero ${superhero.name}`);
